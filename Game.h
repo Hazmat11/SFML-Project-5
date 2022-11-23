eda7fa245b2e56e542b7a5e5f404f9dceb6190c8
Karma <agrandpaul@gaming.tech>
2022-11-23 15:29:04 +0100

    Game and begin for player

---
+++ Game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string.h>
+
+using namespace sf;
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+#define TITLE std::string("The Binding of Pikachu")
+
+void Game();
