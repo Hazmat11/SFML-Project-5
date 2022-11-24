de9f593280faa11ec18c51b7209cf06c36f1e4c5
ebertrand <ebertrand@gaming.tech>
2022-11-24 09:17:12 +0100

    fix bug image

--- src/texture.cpp
+++ src/texture.cpp
@@ -12,13 +12,13 @@ DoTexture::~DoTexture()
 void DoTexture::showTexture() 
 {
 	if (a < 30) {
-		texture.loadFromFile("C:/Users/ebertrand/Documents/SFML/img/player.png", sf::IntRect(10, 10, 60, 60));
+		texture.loadFromFile("img/player.png", sf::IntRect(10, 10, 60, 60));
 		player.setTexture(texture);
 		a++;
 	}
 	
 	if (a < 60 && a > 29) {
-		texture.loadFromFile("C:/Users/ebertrand/Documents/SFML/img/player.png", sf::IntRect(130, 10, 60, 60));
+		texture.loadFromFile("img/player.png", sf::IntRect(140, 10, 60, 60));
 		player.setTexture(texture);
 		a++;
 	}
